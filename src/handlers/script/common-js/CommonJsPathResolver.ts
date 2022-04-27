cb3720472dd8bce5a6bcd8211aaad4f47df60283
Alex Kit <alex.kit@atmajs.com>
2022-04-28 00:34:25 +0200

    publish: add (check ts extension)

--- src/handlers/script/common-js/CommonJsPathResolver.ts
+++ src/handlers/script/common-js/CommonJsPathResolver.ts
@@ -4,72 +4,80 @@ import { io } from '../../../global'
 
 export class CommonJsPathResolver extends BasePathResolver {
 
-	accepts (includeData) {
-		if (includeData.type !== 'js' || includeData.module !== 'commonjs') {
-			return false;
-		}
-		if (!/^[\w\-/]+$/.test(includeData.url)) {
-			return false;
-		}
-		if (nodeCoreModules.indexOf(includeData.url) !== -1) {
-			return false;
-		}
+    accepts (includeData) {
+        if (includeData.type !== 'js' || includeData.module !== 'commonjs') {
+            return false;
+        }
+        if (!/^[\w\-/]+$/.test(includeData.url)) {
+            return false;
+        }
+        if (nodeCoreModules.indexOf(includeData.url) !== -1) {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	resolve (includeData, parentResource) {		
-		var filename = nodeModuleResolve(includeData.url, parentResource.directory);
-		if (filename) {
-			var url = this.solution.opts.toAppUrl(filename);
-			this.solution.opts.mappings[includeData.url] = url;
-		}
-		return filename;
-	}
+    resolve (includeData, parentResource) {
+        let filename = nodeModuleResolve(includeData.url, parentResource.directory);
+        if (filename) {
+            let url = this.solution.opts.toAppUrl(filename);
+            this.solution.opts.mappings[includeData.url] = url;
+        }
+        return filename;
+    }
 };
 
 
 const nodeCoreModules = [
-	'assert', 'buffer', 'child_process', 'cluster', 'console', 'constants', 
-    'crypto', 'dgram', 'dns', 'domain', 'events', 'fs', 'http', 'https', 'module', 'net', 'os', 'path', 
-    'process', 'punycode', 'querystring', 'readline', 'repl', 'stream', 'string_decoder', 'sys', 'timers', 
-	'tls', 'tty', 'url', 'util', 'vm', 'zlib'
+    'assert', 'buffer', 'child_process', 'cluster', 'console', 'constants',
+    'crypto', 'dgram', 'dns', 'domain', 'events', 'fs', 'http', 'https', 'module', 'net', 'os', 'path',
+    'process', 'punycode', 'querystring', 'readline', 'repl', 'stream', 'string_decoder', 'sys', 'timers',
+    'tls', 'tty', 'url', 'util', 'vm', 'zlib'
 ];
 
 function nodeModuleResolve(path, location_){
-	var location = location_.replace(/[\\\/]+$/, '');
-	var name = /^([\w\-]+)/.exec(path)[0];
-	var resource = path.substring(name.length + 1);
-	if (resource && hasExt(resource) === false) {
-		resource += '.js';
-	}
-	var current = location;
-	var root_ = current + '/node_modules/' + name + '/';
-	while (true)  {
-		var nodeModules = path_combine(current, '/node_modules/' + name + '/');
-		var pckg = nodeModules + 'package.json';
-		if (io.File.exists(pckg) === false) {
-			var next = current.replace(/[^\/\\]+[\/\\]?$/, '');
-			if (next === current) {
-				return root_ + 'package.json';
-			}
-			current = next;
-			continue;
-		}
+    let location = location_.replace(/[\\\/]+$/, '');
+    let name = /^([\w\-]+)/.exec(path)[0];
+    let resource = path.substring(name.length + 1);
+    let current = location;
+    let root_ = current + '/node_modules/' + name + '/';
+    while (true)  {
+        let nodeModules = path_combine(current, '/node_modules/' + name + '/');
+        let pckg = nodeModules + 'package.json';
+        if (io.File.exists(pckg) === false) {
+            let next = current.replace(/[^\/\\]+[\/\\]?$/, '');
+            if (next === current) {
+                return root_ + 'package.json';
+            }
+            current = next;
+            continue;
+        }
+        if (resource) {
+            let path = nodeModules + resource;
+            if (hasExt(path) === false) {
+                let likelyTs = path.includes('/src/');
+                if (likelyTs) {
+                    if (io.File.exists(path + '.ts')) {
+                        return path + '.ts';
+                    }
+                }
+                return path + '.js';
+            }
+            return path;
+        }
+        let json = io.File.read(pckg);
+        if (typeof json === 'string') {
+            json = JSON.parse(json);
+        }
 
-		var json = io.File.read(pckg);
-		if (typeof json === 'string') {
-			json = JSON.parse(json);
-		}
-		if (resource) {
-			return nodeModules + resource;
-		}
-		if (json.main) {
-			return path_combine(nodeModules, json.main)
-		}		
-		return nodeModules + 'index.js';
-	}
+        if (json.main) {
+            return path_combine(nodeModules, json.main)
+        }
+
+        return nodeModules + 'index.js';
+    }
 }
 function hasExt(path) {
-	return /\.[\w]{1,8}($|\?)/.test(path);
+    return /\.[\w]{1,8}($|\?)/.test(path);
 }
